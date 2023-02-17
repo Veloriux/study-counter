bdcecb8bc0ce64cccbb8bad6a7ed8ae2399f2596
David King <117994855+davidbkingjr@users.noreply.github.com>
2023-02-17 02:23:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-let loggedHours = []
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+let loggedHours = [];
 
 function increment() {
-    count += 1
-    countEl.textContent = count
-}
+    count += 1;
+    countEl.textContent = count;
+};
 
 function save() {
     loggedHours.push(count)
     for (let i = 0; i < loggedHours.length; i++) {
-        saveEl.textContent = loggedHours
-        countEl.textContent = 0
-        count = 0
-    }
-}
+        saveEl.textContent = loggedHours;
+        countEl.textContent = 0;
+        count = 0;
+    };
+};
 
 // function reset() {
 //     loggedHours = []
